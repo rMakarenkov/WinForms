a2a3a9c30bd72a50f07874132f1197f9f4c4acc8
rMakarenkov <romgover@gmail.com>
2022-08-15 22:04:34 +0300

    Forms clone

---
+++ WinFormsInheritance/WinFormsInheritance/Form3.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsInheritance
+{
+	public partial class Form3 : Form2
+	{
+		public Form3()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			GetColorForm1Method();
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			GetColorForm2Method();
+		}
+	}
+
+}
